c7136f5e3b402ed6aec82720e5665ec33267f0c4
Breno Araújo de Matos <97618949+brenusaraujo@users.noreply.github.com>
2025-10-15 08:32:05 -0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,10 +1,17 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import App from './App';
+import Reembolso from './Components/Reembolso';
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
+const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Router>
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="/reembolso" element={<Reembolso />} />
+      </Routes>
+    </Router>
   </React.StrictMode>
 );
