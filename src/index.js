10d020791d181974182ab63d4576b97027314135
Breno Araújo de Matos <97618949+brenusaraujo@users.noreply.github.com>
2025-10-15 08:45:46 -0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import App from './App';
 import Reembolso from './Components/Reembolso';
+import Produto from './Components/Produto';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -11,6 +12,7 @@ root.render(
       <Routes>
         <Route path="/" element={<App />} />
         <Route path="/reembolso" element={<Reembolso />} />
+        <Route path="/produto/:id" element={<Produto />} />
       </Routes>
     </Router>
   </React.StrictMode>
